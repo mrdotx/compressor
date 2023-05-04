663d5e845c56eb172602f8a166c7ce82c876cac0
mrdotx <klassiker@gmx.de>
2023-05-04 07:58:50 +0200

    option to compress pdfs added

--- compressor.sh
+++ compressor.sh
@@ -3,13 +3,12 @@
 # path:   /home/klassiker/.local/share/repos/compressor/compressor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/compressor
-# date:   2022-08-07T11:07:49+0200
+# date:   2023-05-03T19:19:45+0200
 
 check() {
-    tools="7z bzip2 gzip tar unrar unzip xz zip zstd"
+    tools="7z bzip2 ghostscript gzip tar unrar unzip xz zip zstd "
 
-    printf "required tools for full functionality. tools marked with an X are installed\n"
-
+    printf "\n"
     for tool in $tools; do
         if command -v "$tool" > /dev/null 2>&1; then
             printf "      [X] %s\n" "$tool"
@@ -30,15 +29,28 @@ help="$script [-h/--help] -- script to compress/extract files and folders
              exe, gz, iso, lzh, lzma, msi, pkg, rar, rpm, tar, tar.bz2, tar.gz,
              tar.xz, tar.zst, tbz2, tgz, txz, udf, wim, xar, xz, z, zip
 
+    $script [--pdf] <quality> <path/file>.pdf [path/file1.pdf] [path/file2.pdf]
+      <quality>: screen, ebook, printer, prepress, default
+
   Settings:
     [--add] = compress files to archive
+    [--pdf] = compress pdf files with the following quality settings:
+                screen   = low-resolution
+                ebook    = medium-resolution
+                printer  = high-resolution
+                prepress = very high-resolution
+                default  = useful across a wide variety of uses
+                           (possible larger file size)
+              (if no quality settings are specified, \"default\" is used)
 
   Examples:
-    $script --add archive.tar.gz file1.ext file2.ext file3.ext
     $script file1.tar.gz file2.tar.bz2 file3.7z
+    $script --add archive.tar.gz file1.ext file2.ext file3.ext
+    $script --pdf ebook document1.pdf document2.pdf document3.pdf
 
   Programs:
-    $(check)"
+    required tools for full functionality: $(check)
+    tools marked with an X are installed"
 
 compress() {
     archive="$1"
@@ -148,6 +160,29 @@ extract() {
     printf "processes completed\n"
 }
 
+compress_pdf() {
+    case $1 in
+        screen | ebook | printer | prepress | default)
+            quality="$1"
+            shift
+            ;;
+        *)
+            quality="default"
+            ;;
+    esac
+
+    for document in "$@"; do
+        ghostscript \
+            -sDEVICE=pdfwrite \
+            -dPDFSETTINGS=/"$quality" \
+            -dPrinted=false \
+            -dNOPAUSE \
+            -dBATCH \
+            -sOutputFile="$(basename "$document" .pdf)-$quality.pdf" \
+            "$document"
+    done
+}
+
 case "$1" in
     -h | --help | "")
         printf "%s\n" "$help"
@@ -155,6 +190,10 @@ case "$1" in
     --add)
         shift
         compress "$@"
+        ;;
+    --pdf)
+        shift
+        compress_pdf "$@"
         ;;
     *)
         extract "$@"
